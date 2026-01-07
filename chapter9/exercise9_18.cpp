bc86aa270691be7401f9a98e24d72bcce5bed236
Pham Hoang Minh - AI Engineer Lead <minh.pham@ndsvn.vn>
2026-01-07 17:37:51 +0700

    updates

---
+++ chapter9/exercise9_18.cpp
@@ -0,0 +1,14 @@
+// Write a program to read a sequence of strings from <cin> to <deque>
+// Use <iterator>s to print elements in <deque>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+
+  return 0;
+}
