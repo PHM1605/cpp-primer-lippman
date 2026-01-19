12140dc637a6dcef05e8818dbf13970a179ad943
PHM1605 <PHM1605@gmail.com>
2026-01-19 11:28:38 +0700

    updates

---
+++ chapter12/exercise12_6.cpp
@@ -0,0 +1,4 @@
+// Write a function that returns a dynamically allocated vector of ints
+// Pass that vector to a function that reads <cin> to give values
+// Pass that vector to another function to print values
+// (remember to delete properly)
